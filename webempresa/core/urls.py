bee607e43d40d621bd376352da396844b4c6ee93
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-26 16:27:59 +0100

    Tercera App [Services] Modelo y admin

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path('', views.home, name="home"),
- path('home/', views.home, name="home"),
  path('about/', views.about, name="about"),
  path('services/', views.services, name="services"),
  path('store/', views.store, name="store"),
--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -20,3 +20,9 @@ urlpatterns = [
  path('', include('core.urls')),
  path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+if settings.DEBUG:
+ from django.conf.urls.static import static
+ urlpatterns += static(settings.MEDIA_URL,
+ document_root=settings.MEDIA_ROOT)
