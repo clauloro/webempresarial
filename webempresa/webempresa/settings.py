5bc94542f1feac62728c5e51f8b264e4b65ec9d3
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-27 15:43:24 +0100

    Quinta App Social/Redes sociales

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'services.apps.ServicesConfig',
-    'blog'
+    'blog',
+    'social.apps.SocialConfig'
 ]
 
 MIDDLEWARE = [
