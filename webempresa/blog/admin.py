6f2b5a668bca1c51ebac9515d92a5cd386609a14
clauloro <114728388+clauloro@users.noreply.github.com>
2024-11-30 05:08:42 +0100

    ultimas modificaciones

--- webempresa/blog/admin.py
+++ webempresa/blog/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 from .models import Category, Post
+
+# Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
- readonly_fields = ('created', 'updated')
+    readonly_fields = ('created', 'updated')
 class PostAdmin(admin.ModelAdmin):
- readonly_fields = ('created', 'updated')
- list_display = ('title', 'author', 'published', 'post_categories')
- ordering = ('author', 'published')
- search_fields = ('title','content','author__username','categories__name')
- date_hierarchy = 'published'
- list_filter = ('author__username','categories__name')
- def post_categories(self, obj):
-    return ", ".join(
-        [c.name for c in obj.categories.all().order_by("name")])
- post_categories.short_description = "Categorías"
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published', 'post_categories') # Muestra los campos en la lista de entradas.
+    ordering = ('author', 'published') # Ordena las entradas por autor y fecha de publicación.
+    search_fields = ('title','content','author__username', 'categories__name') # Campos por los que se puede buscar.
+    date_hierarchy = 'published' # Jerarquía de fechas.
+    list_filter = ('author__username','categories__name') # Filtros de búsqueda.
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+    post_categories.short_description = "Categorías"
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
 
 
 
--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
+# URL configuration for webempresa project (blog application).
 urlpatterns = [
- path('', views.blog, name="blog"),
- path('category/<int:category_id>/', views.category, name="category"),
- ]
+    path('', views.blog, name="blog"),
+    path('category/<int:category_id>/', views.category, name="category"),
+]
 
